8ea624ea3f5f7b62cfd67a508650b9cdd9f43f0e
n1ks2n <nikita.pimoshenko@gmail.com>
2018-01-23 20:55:24 +0100

    Base test-task structure

---
+++ src/Controller/ApiDataController.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace TestTask\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiDataController extends Controller
+{
+    /**
+     * @Route("/api/data")
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function index()
+    {
+        $response = json_decode(file_get_contents(__DIR__ . '/apiResource/data.json'));
+
+        return $this->json($response);
+    }
+}
